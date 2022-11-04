9246c8b6de8768b214fe37e39cf895e4ff021ca0
Ganesh K Bhat <ganeshsurfs@gmail.com>
2022-11-04 07:37:45 +0530

    [TEMP] Minor commit to exports of requests.js

--- index.mjs
+++ index.mjs
@@ -19,4 +19,4 @@
 import { _isValidURL, _getProtocol, _checkHttpsProtocol, _fetch, _deleteRequest, _getRequest, _postRequest, _putRequest, _patchRequest, _request } from "./index.js";
 
 export default _request;
-export { _isValidURL, _getProtocol, _checkHttpsProtocol, _getRequest, _fetch, _deleteRequest, _getRequest, _postRequest, _putRequest, _patchRequest, _request };
+export { _isValidURL, _getProtocol, _checkHttpsProtocol, _fetch, _deleteRequest, _getRequest, _postRequest, _putRequest, _patchRequest, _request };
